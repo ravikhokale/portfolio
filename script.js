f0c65f276c3dbb329f877bc8371fae546358d764
Ravi Khokale <ravi_khokale_mca@moderncoe.edu.in>
2026-02-26 10:39:22 +0530

    Add script.js for portfolio interactivity

---
+++ script.js
@@ -0,0 +1,8 @@
+// portfolio interactivity
+
+function showCurrentDateTime() {
+    const currentDateTime = new Date().toUTCString();
+    console.log(`Current Date and Time (UTC): ${currentDateTime}`);
+}
+
+showCurrentDateTime();
